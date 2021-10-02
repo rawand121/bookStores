b0bccf0b38780b118df4417af971eb729b978793
Rawand121 <58867322+rawand121@users.noreply.github.com>
2021-10-03 01:37:32 +0300

    Update layout.js

--- Components/layout/layout.js
+++ Components/layout/layout.js
@@ -6,8 +6,7 @@ import { useEffect } from "react";
 import { useDispatch } from "react-redux";
 import { LoadUser } from "../../redux/actions/index";
 import "react-toastify/dist/ReactToastify.css";
-import Script from 'next/script'
-
+ 
 const Layout = (props) => {
   const { locale } = useRouter();
   const dispatch = useDispatch();
@@ -30,11 +29,11 @@ const Layout = (props) => {
           integrity="sha384-KyZXEAg3QhqLMpG8r+8fhAXLRk2vvoC2f3B09zVXn8CA5QIVfZOJ3BCsw2P0p/We"
           crossOrigin="anonymous"
         />
-        <Script
+        <script
           src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.0/dist/js/bootstrap.bundle.min.js"
           integrity="sha384-U1DAWAznBHeqEIlVSCgzq+c9gqGAJn5c/t99JyeKa9xxaYpSvHU5awsuZVVFIhvj"
           crossOrigin="anonymous"
-        ></Script>
+        ></script>
 
         <link
           rel="stylesheet"
