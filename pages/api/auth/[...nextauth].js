37da284fde9d09e26c22e73975308f0e308f8f82
rawand121 <RawandRebwar2010@gmail.com>
2021-10-03 03:29:43 +0300

    Logo Added

--- pages/api/auth/[...nextauth].js
+++ pages/api/auth/[...nextauth].js
@@ -5,7 +5,7 @@ import User from "../../../Backend/models/user";
 import BookStoreModel from "../../../Backend/models/bookStore";
 import dbConnection from "../../../Backend/config/dbConfig";
 import bcrypt from "bcrypt";
-import cors from 'cors'
+import cors from "cors";
 
 export default NextAuth({
   session: {
@@ -24,7 +24,14 @@ export default NextAuth({
 
         let endpoint = await User.findOne({ email }).select("+password");
 
-        if (credentials.callbackUrl === "https://book-stores.vercel.app/signin-store") {
+        if (
+          credentials.callbackUrl ===
+            "https://book-stores.vercel.app/signin-store" ||
+          credentials.callbackUrl ===
+            "https://book-stores.vercel.app/Arabic/signin-store" ||
+          credentials.callbackUrl ===
+            "https://book-stores.vercel.app/Kurdish/signin-store"
+        ) {
           endpoint = await BookStoreModel.findOne({ email });
         }
 
