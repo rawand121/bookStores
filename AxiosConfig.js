7fa5051717a9fdf62ac1c6db6cb5a11f27547e7f
rawand121 <RawandRebwar2010@gmail.com>
2021-10-04 17:21:54 +0300

    changed baseUrl

--- AxiosConfig.js
+++ AxiosConfig.js
@@ -1,8 +1,8 @@
 import axios from "axios";
 
 const instance = axios.create({
-    // baseURL : 'https://book-stores.vercel.app'
-    baseURL : 'http://localhost:3000'
+    baseURL : 'https://book-stores.vercel.app'
+    // baseURL : 'http://localhost:3000'
 })
 
 export default instance
