6f7ea9666aa6857c95d3ed9def517c0447f6e85a
Rawand121 <58867322+rawand121@users.noreply.github.com>
2021-10-28 13:05:22 +0300

    Update header.jsx

--- Components/HomeHeader/header.jsx
+++ Components/HomeHeader/header.jsx
@@ -3,7 +3,7 @@ import classes from './header.module.css'
 const header = () => {
     return (
         <div>
-            <img className={classes.bgImg} src="./images/Background.jpg" />
+            <img className={classes.bgImg} src="https://images.unsplash.com/photo-1544716278-e513176f20b5?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=1074&q=80" />
         </div>
     );
 }
