2d58c910c27e64a56a1b9bc96927f939517d05a2
Rawand121 <58867322+rawand121@users.noreply.github.com>
2021-11-11 20:57:59 +0300

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -4,13 +4,6 @@ module.exports = {
     locales: ["English", "Kurdish", "Arabic"],
     defaultLocale: "English",
   },
-  env: {
-    MONGO_URL:
-      "mongodb+srv://My1stDB:Messi2011@cluster0.er8jm.mongodb.net/Bookstores?retryWrites=true&w=majority",
-    CLOUDINARY_NAME: "rawand121",
-    API_KEY: "184712644846138",
-    API_SECRET: "SmlHIPa1k_xSNLWy0PxcN803yQI",
-  },
   images: {
     domains: ["res.cloudinary.com"],
   },
